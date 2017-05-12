8a4bfdf04a7f99fd463bec8273abe63842854aa9
bluezery <ohpowel@gmail.com>
2017-05-12 14:47:10 +0900

    util: Add process name in the logging

--- util/nemoutil-log.h
+++ util/nemoutil-log.h
@@ -10,12 +10,15 @@
     #define __UNUSED__
 #endif
 
+#define __GNU_SOURCE
+extern char *program_invocation_name;
+
 #define LOG_COLOR_BLUE "\x1b[34m"
 #define LOG_COLOR_RED "\x1b[31m"
 #define LOG_COLOR_GREEN "\x1b[32m"
 #define LOG_COLOR_END "\x1b[0m"
 
-#define LOGFMT(COLOR, fmt, ...) do { fprintf(stderr, "[%s:%d][%s] " fmt "\n", __FILE__,  __LINE__, __func__, ##__VA_ARGS__); } while (0)
+#define LOGFMT(COLOR, fmt, ...) do { fprintf(stderr, "%s:[%s:%d][%s] " fmt "\n", program_invocation_name, __FILE__,  __LINE__, __func__, ##__VA_ARGS__); } while (0)
 #define ERR(...) LOGFMT(LOG_COLOR_RED, ##__VA_ARGS__)
 #define LOG(...) LOGFMT(LOG_COLOR_GREEN, ##__VA_ARGS__)
 
