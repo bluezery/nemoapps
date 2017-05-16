7c4917b2a8e370f286b757e515ec41d05c2db220
bluezery <ohpowel@gmail.com>
2017-05-16 20:48:26 +0900

    util: Add string util

---
+++ util/nemoutil-string.c
@@ -0,0 +1,25 @@
+#include "nemoutil-internal.h"
+#include "nemoutil.h"
+#include "nemoutil-string.h"
+
+size_t string_count_utf(const char *str)
+{
+    RET_IF(!str, 0);
+
+    size_t cnt = 0;
+    size_t len = 0;
+    while (str[len]) {
+        if (cnt >= 10) break;
+        if ((str[len] & 0xF0) == 0xF0)  { // 4 bytes
+            len+=4;
+        } else if ((str[len] & 0xE0) == 0xE0) { // 3 bytes
+            len+=3;
+        } else if ((str[len] & 0xC0) == 0xC0) { // 2 bytes
+            len+=2;
+        } else { // a byte
+            len+=1;
+        }
+        cnt++;
+    }
+    return cnt;
+}
---
+++ util/nemoutil-string.h
@@ -0,0 +1,17 @@
+#ifndef __UTIL_STRING_H__
+#define __UTIL_STRING_H__
+
+#include <nemoutil-list.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t string_count_utf(const char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
